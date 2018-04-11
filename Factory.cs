a3dbf6710329568f8702d30a2fbe512c0d729d50
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-04-11 16:55:23 +0700

    initial

---
+++ Factory.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bogus;
+
+namespace Bunnypro.SimpleFactory
+{
+    public class Factory<T>
+    {
+        private readonly Func<Faker, object> _generator;
+        private readonly Faker _faker;
+
+        public Factory(Func<Faker, object> generator)
+        {
+            _generator = generator;
+            _faker = new Faker();
+        }
+
+        public T CreateOne(Func<T, Faker, T> extender)
+        {
+            return extender(CreateOne(), _faker);
+        }
+
+        public T CreateOne()
+        {
+            return Create().First();
+        }
+
+        public IEnumerable<T> Create(int count, Func<T, Faker, T> extender)
+        {
+            return Create(count).Select(o => extender(o, _faker));
+        }
+
+        public IEnumerable<T> Create(int count = 1)
+        {
+            if (count < 1)
+            {
+                throw new Exception("Minimum Factory Create is One");
+            }
+
+            try
+            {
+                return Enumerable.Range(0, count).Select(_ => (T)_generator(_faker));
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public IEnumerable<T> CreateUnique(int count, Func<T, Faker, T> extender)
+        {
+            return CreateUnique(count).Select(o => extender(o, _faker));
+        }
+
+        public IEnumerable<T> CreateUnique(int count = 1)
+        {
+            if (count < 1)
+            {
+                throw new Exception("Minimum Factory Create is One");
+            }
+
+            try
+            {
+                var data = new List<T>();
+
+                T GenerateUnique(Func<Faker, object> generator)
+                {
+                    var o = (T)generator(_faker);
+
+                    return data.Contains(o) ? GenerateUnique(generator) : o;
+                }
+
+                for (var i = 0; i < count; i++)
+                {
+                    data.Add(GenerateUnique(_generator));
+                }
+
+                return data;
+            }
+            catch (KeyNotFoundException e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+    }
+
+    public class Factory
+    {
+        private static readonly Dictionary<Type, Func<Faker, object>> Generators = new Dictionary<Type, Func<Faker, object>>();
+
+        public static Factory<T> Register<T>(Func<Faker, object> generator)
+        {
+            Generators.Add(typeof(T), generator);
+
+            return Once<T>(generator);
+        }
+
+        public static bool Remove<T>()
+        {
+            try
+            {
+                return Generators.Remove(typeof(T));
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public static Factory<T> Once<T>(Func<Faker, object> generator)
+        {
+            return new Factory<T>(generator);
+        }
+
+        public static T CreateOne<T>(Func<T, Faker, T> extender)
+        {
+            try
+            {
+                return new Factory<T>(Generators[typeof(T)]).CreateOne(extender);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public static T CreateOne<T>()
+        {
+            try
+            {
+                return new Factory<T>(Generators[typeof(T)]).CreateOne();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public static IEnumerable<T> Create<T>(int count, Func<T, Faker, T> extender)
+        {
+            try
+            {
+                return new Factory<T>(Generators[typeof(T)]).Create(count, extender);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public static IEnumerable<T> Create<T>(int count = 1)
+        {
+            try
+            {
+                return new Factory<T>(Generators[typeof(T)]).Create(count);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public IEnumerable<T> CreateUnique<T>(int count, Func<T, Faker, T> extender)
+        {
+            try
+            {
+                return new Factory<T>(Generators[typeof(T)]).CreateUnique(count, extender);
+            }
+            catch (KeyNotFoundException e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+
+        public static IEnumerable<T> CreateUnique<T>(int count = 1)
+        {
+            try
+            {
+                return new Factory<T>(Generators[typeof(T)]).CreateUnique(count);
+            }
+            catch (KeyNotFoundException e)
+            {
+                throw new Exception("Factory for " + typeof(T) + " is not registered", e);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,68 @@
+# Dotnet Simple Object Factory
+
+This library provide a simple object factory for generating data using Bogus as fake data generator.
+
+```c#
+using Bunnypro.SimpleFactory;
+
+// Register Factory
+Factory.Register<Person>(faker => new Person(
+{
+    FullName = faker.Person.FullName,
+    Address = faker.Person.Address
+}));
+
+// Unregister Factory
+Factory.Remove<Person>();
+
+// Generate Data
+var people = Factory.Create<Person>(4); // return IEnumerable<Person>
+
+// Generate One Data
+var person = Factory.CreateOne<Person>(); // return Person
+
+// Extending Data
+var people = Factory.Create<Person>(4, (person, faker) =>
+{
+    person.Email = faker.Person.Email;
+
+    return person;
+});
+
+// Register With Generate Nested Data
+Factory.Register<Schedule>(faker => new Schedule(
+{
+    People = Factory.Create<Person>(4).ToList(),
+    Date = faker.Date
+}));
+
+// Generate Without Register
+Factory
+    .Once<double>(
+        faker => Math.Round(faker.Random.Double(2, 4), 2)
+    ).Create(4).ToArray(); // Generate 4 double in two decimal places
+
+
+// Or Another Way
+var DoubleFactory = new Factory<double>(
+    faker => Math.Round(faker.Random.Double(2, 4), 2)
+);
+
+var doubles = DoubleFactory.Create(4).ToArray();
+
+var PersonFactory = new Factory<Person>(faker => new Person(
+{
+    FullName = faker.Person.FullName,
+    Address = faker.Person.Address
+}));
+
+// same as
+
+var PersonFactory = Factory.Once<Person>(faker => new Person(
+{
+    FullName = faker.Person.FullName,
+    Address = faker.Person.Address
+}));
+
+var people = PersonFactory.Create(100);
+```
